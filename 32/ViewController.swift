8d5891adfb9d04f86496cf79d646971d726971ea
Mert Serkan Aydın <mert-serkan-01@hotmail.com>
2021-05-10 10:49:41 +0300

    Initial Commit

---
+++ 32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  32
+//
+//  Created by Mert Serkan Aydın on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
